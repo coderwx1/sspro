ba8e7df379ce76ee57213f767fa689f2ff7502a1
freedomwebcn <freedomweb@yeah.net>
2023-07-03 18:44:47 +0800

    添加笔记

--- src/App.vue
+++ src/App.vue
@@ -1,138 +1,64 @@
 <template>
-  <div class="home">
-    <div class="btns">
-      <div class="eml-cell">
-        <van-cell-group>
-          <van-field
-            v-model="eml"
-            label="邮箱"
-            size="large"
-            placeholder="请输入邮箱"
-          />
-        </van-cell-group>
-      </div>
-      <div class="btn">
-        <van-button type="primary " size="large" @click="sendCode">
-          发送验证码
-        </van-button>
-      </div>
-      <div class="captcha-cell">
-        <van-cell-group>
-          <van-field
-            v-model="captcha"
-            label="验证码"
-            size="large"
-            placeholder="请输入验证码"
-          />
-        </van-cell-group>
-      </div>
-      <div class="btn">
-        <van-button type="primary" size="large" @click="register">
-          注册
-        </van-button>
-      </div>
-      <div class="btn">
-        <van-button type="primary" size="large" @click="login">
-          登陆
-        </van-button>
-      </div>
-      <div class="btn">
-        <van-button type="danger" size="large" @click="killUser">
-          删除用户
-        </van-button>
-      </div>
-      <div class="btn">
-        <van-button type="primary" size="large" @click="buy"> 购买 </van-button>
-      </div>
+  <div class="app">
+    <div class="note-wrapper">
+      <ul>
+        <li>
+          <h2>note 1</h2>
+          <p>
+            第一、对于减法而言，减数和被减数翻转以后，得到的新的差和原来的差是互为相反数的。与之对应，在除法当中，如果除数和被除数的位置颠倒了，得到的商和原来的商就互为倒数。<br />
+            第二、如果一个数字要减去多个数，减数的位置可以任意移动；如果一个数字要连续除以多个数，除数的位置也可以任意移动。<br />
+            第三、即使减法和加法混合运算，它们的位置也是可以随意调整的，因为我们可以把减去一个正数看作是加上一个负数。同样，除法有类似的性质。<br />
+            在一个加减法混合运算的算式中，如果是减数移动到算式的第一位，那么应该把减数变为它的相反数。同理，一个由乘除法组成的混合算式，如果一个除数移动到算式的第一位，那么除数应该变成自己的倒数。
+          </p>
+          <hr />
+          <div class="source-wrapper">
+            <span class="source-text">
+              note 1 来源：《学好数学并不难》上册 第七章 减法和除法的独特优势：三个共同点
+            </span>
+          </div>
+        </li>
+      </ul>
     </div>
-    <div class="res-Text">{{ resText }}</div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-
-const BASEURL = `https://vercel-python-sepia-tau.vercel.app`;
-let resText = ref("");
-let eml = ref("freedomweb@yeah.net");
-let captcha = ref("");
-
-function sendCode() {
-  if (!eml.value) return;
-  const url = `${BASEURL}/send_captcha/${eml.value}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      resText.value = data.msg;
-    });
-}
-
-function register() {
-  if (!eml.value) return;
-  const url = `${BASEURL}/register/${eml.value}/${captcha.value}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      resText.value = data.msg;
-      console.log(data);
-    });
-}
-
-function login() {
-  if (!eml.value) return;
-  const url = `${BASEURL}/login/${eml.value}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      resText.value = data.msg;
-      console.log(data);
-    });
-}
-
-function killUser() {
-  if (!eml.value) return;
-  const url = `${BASEURL}/kill_user/${eml.value}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      resText.value = data.msg;
-      console.log(data);
-    });
-}
-
-function buy() {
-  const url = `${BASEURL}/buy`;
-  fetch(url)
-    .then((response) => response.text())
-    .then((data) => {
-      resText.value = data;
-      console.log(data);
-    });
-}
 </script>
 
 <style scoped>
-.home {
+.app {
   width: 375px;
   height: 100%;
-  background-color: #f7f8fa;
 }
-.btns {
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-  padding: 20px 10px;
-}
-.btn,
-.eml-cell,
-.captcha-cell {
-  padding: 10px 0;
+.note-wrapper {
+  padding: 20px;
+  letter-spacing: 1.5px;
+  text-align: justify;
 }
 
-.res-Text {
-  padding: 0 10px;
-  text-align: center;
-  word-wrap: break-word;
+h2 {
+  font-size: 25px;
+  color: rgb(247, 17, 93);
+  padding: 0 0 6px 0;
+  margin-bottom: 10px;
+  border-bottom: 1px solid black;
+}
+p {
+  font-size: 16px;
+}
+hr {
+  border: none;
+  height: 1px;
+  background-color: black;
+  margin: 10px 0;
+}
+.source-wrapper {
+  /* text-align: right; */
+}
+.source-text {
+  font-size: 13px;
+  /* rgb(255, 0, 179) */
+  color: rgb(241, 18, 18);
 }
 </style>
