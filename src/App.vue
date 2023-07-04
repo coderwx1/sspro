d4666b8c038ae590c938b54e3cf11298b6988bb0
freedomwebcn <freedomweb@yeah.net>
2023-07-05 06:54:22 +0800

    add note count

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <li>
           <h2>Note 1</h2>
           <div>
-            <p >
+            <p>
               第一、对于减法而言，减数和被减数翻转以后，得到的新的差和原来的差是互为相反数的。与之对应，在除法当中，如果除数和被除数的位置颠倒了，得到的商和原来的商就互为倒数。<br />
               第二、如果一个数字要减去多个数，减数的位置可以任意移动；如果一个数字要连续除以多个数，除数的位置也可以任意移动。<br />
               第三、即使减法和加法混合运算，它们的位置也是可以随意调整的，因为我们可以把减去一个正数看作是加上一个负数。同样，除法有类似的性质。<br />
@@ -14,8 +14,12 @@
               在一个加减法混合运算的算式中，如果是减数移动到算式的第一位，那么应该把减数变为它的相反数。同理，一个由乘除法组成的混合算式，如果一个除数移动到算式的第一位，那么除数应该变成自己的倒数。
             </p>
           </div>
+          <div class="divider-wrapper">
+            <div class="divider">
+              <i class="note-count">1</i>
+            </div>
+          </div>
 
-          <hr />
           <div class="source-wrapper">
             <span class="source-content">
               Note 1 来源：《学好数学并不难》上册 第七章 第四节
@@ -29,21 +33,27 @@
           <div>
             <p>
               同底数幂相乘，底数不变，指数相加。
-              <span class="formula"> 公式：\(a^m \ × \ a^n \ = \ a^{m+n} \) </span>
+              <span class="formula">
+                公式：\(a^m \ × \ a^n \ = \ a^{m+n} \)
+              </span>
             </p>
             <p>
               同底数的幂相除，底数不变，指数相减。<br />
-              <span class="formula"> 公式：\(a^m \ ÷ \ a^n \ = \ a^{m-n} \) </span>
+              <span class="formula">
+                公式：\(a^m \ ÷ \ a^n \ = \ a^{m-n} \)
+              </span>
             </p>
-            <p>
-              当指数是负数的时候，结果等于正数幂的倒数。
-            </p>
+            <p>&nbsp当指数是负数的时候，结果等于正数幂的倒数。</p>
             <p>
               任何数的1次方都等于它自己，而除了0以外的任何数的0次方都等于1。
             </p>
           </div>
 
-          <hr />
+          <div class="divider-wrapper">
+            <div class="divider">
+              <i class="note-count">2</i>
+            </div>
+          </div>
           <div class="source-wrapper">
             <span class="source-content">
               Note 2 来源：《学好数学并不难》上册 第十章 第二节
@@ -57,19 +67,24 @@
           <div>
             <p>
               同指数的幂相乘，指数不变，底数相乘。<br />
-              <span class="formula"> 公式：\(a^n \ × \ b^n \ = \ ( \ ab \ )^n \) </span>
+              <span class="formula">
+                公式：\(a^n \ × \ b^n \ = \ ( \ ab \ )^n \)
+              </span>
               <span class="example">
                 由于乘方的结果叫幂，相乘的结果叫积，因此这个口诀也被简称为幂的积等于积的幂，同理，积的幂也等于幂的积。
               </span>
               <br />
               <span class="example">
-                例题：\((2x)^4 = ? \)，因为2\(x\)是2和\(x\)的乘积，积的幂等于幂的积，因此\((2x)^4 = 2^4 × x^4 \)，由于2的4次方等于16，因此结果为\(16x^4 \)。
+                例题：\((2x)^4 = ?
+                \)，因为2\(x\)是2和\(x\)的乘积，积的幂等于幂的积，因此\((2x)^4 =
+                2^4 × x^4 \)，由于2的4次方等于16，因此结果为\(16x^4 \)。
               </span>
             </p>
             <p>
               商的幂等于幂的商。<br />
               <span class="example">
-                例题：<i class="fraction-fz">\( ({3 \over x }) ^2 \)，</i> 此题处理的是分数的乘方，商的幂等于幂的商，直接把2次方扔到分子分母上，得到: 
+                例题：<i class="fraction-fz">\( ({3 \over x }) ^2 \)，</i>
+                此题处理的是分数的乘方，商的幂等于幂的商，直接把2次方扔到分子分母上，得到:
                 <i class="fraction-fz">
                   \({3^2 \over x^2} = {9 \over x^2} \)
                 </i>
@@ -77,16 +92,27 @@
             </p>
             <p>
               幂的乘方，底数不变，指数相乘。<br />
-              <span class="formula"> 公式：\(( \ a^m \ ) \ ^n \ = \ a^{m \ n}\) </span> <br />
+              <span class="formula">
+                公式：\(( \ a^m \ ) \ ^n \ = \ a^{m \ n}\)
+              </span>
+              <br />
               <span class="example">
-                例题：\((4y^2)^3 \ = \ ? \)，解决问题的关键是要把系数4搞清楚，要知道 \( \ 4y^2 \ = \ 4 × y × y \)，所以y的指数2，这个2是y自我相乘的次数，跟系数4没关系，但是整个算式的3次方，既是4的3次方，也是 \(y^2\)
-                的3次方，整个算式符合积的幂的运算法则，因此原式等于\(4^3 × (y^2)^3 \) ，后者符合幂的乘方法则，指数相乘得到\(4^3 × y^{2×3}\)
-                ，进一步化简，得到\(64y^6\)。
+                例题：\((4y^2)^3 \ = \ ?
+                \)，解决问题的关键是要把系数4搞清楚，要知道 \( \ 4y^2 \ = \ 4 ×
+                y × y
+                \)，所以y的指数2，这个2是y自我相乘的次数，跟系数4没关系，但是整个算式的3次方，既是4的3次方，也是
+                \(y^2\) 的3次方，整个算式符合积的幂的运算法则，因此原式等于\(4^3
+                × (y^2)^3 \) ，后者符合幂的乘方法则，指数相乘得到\(4^3 ×
+                y^{2×3}\) ，进一步化简，得到\(64y^6\)。
               </span>
             </p>
           </div>
 
-          <hr />
+          <div class="divider-wrapper">
+            <div class="divider">
+              <i class="note-count">3</i>
+            </div>
+          </div>
           <div class="source-wrapper">
             <span class="source-content">
               Note 3 来源：《学好数学并不难》上册 第十章 第三节
@@ -95,7 +121,7 @@
           </div>
         </li>
 
-          <li>
+        <li>
           <h2>Note 4</h2>
           <div>
             <p>
@@ -103,13 +129,21 @@
             </p>
             <p>
               不等式左右两边同时乘以或除以一个负数，不等号的方向必须要发生改变。<br />
-              <span class="example"> 证明：2 > －4，如果不等式两边同时乘以－1会怎么样？左边变成了－2，右边呢，变成了4，－2是小于4的，这时候我们就发现，同乘一个负数，大于号变成了小于号．同样，如果我们把－2＜4这个不等式左右两边同时除以－2，左边就是1，右边是－2，左边就大于右边了。 </span>
+              <span class="example">
+                证明：<i class="example-question">2 > -4</i>
+                ，如果不等式两边同时乘以－1会怎么样？左边变成了－2，右边呢，变成了4，－2是小于4的，这时候我们就发现，同乘一个负数，大于号变成了小于号．同样，如果我们把－2＜4这个不等式左右两边同时除以－2，左边就是1，右边是－2，左边就大于右边了。
+              </span>
             </p>
           </div>
-          <hr />
+          <div class="divider-wrapper">
+            <div class="divider">
+              <i class="note-count">4</i>
+            </div>
+          </div>
           <div class="source-wrapper">
             <span class="source-content">
-              Note 4 来源：《学好数学并不难》上册 第六章 第四节 不等式的性质：方向的改变
+              Note 4 来源：《学好数学并不难》上册 第六章 第四节
+              不等式的性质：方向的改变
             </span>
           </div>
         </li>
@@ -119,6 +153,8 @@
 </template>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
+
 .app {
   width: 375px;
   height: 100%;
@@ -135,7 +171,7 @@ h2 {
   /* rgb(247, 17, 93) */
   color: rgb(238, 32, 101);
   padding: 0 0 6px 0;
-  margin-bottom: 15px;
+  margin-bottom: 24px;
   border-bottom: 3px solid rgb(245, 55, 118);
 }
 p {
@@ -151,11 +187,24 @@ p::before {
   background-color: black;
   border-radius: 50%;
 }
-hr {
-  border: none;
+.divider-wrapper {
+  padding: 20px 0;
+  font-size: 20px;
+  font-family: "Lobster", cursive;
+  color: rgb(235, 38, 159);
+}
+
+.divider {
   height: 1px;
   background-color: black;
-  margin: 10px 0;
+  position: relative;
+}
+
+.note-count {
+  position: absolute;
+  right: 0;
+  top: 0;
+  transform: translateY(-100%);
 }
 
 .formula {
@@ -167,7 +216,7 @@ hr {
   background-color: #f8f8f8;
   margin: 10px 0;
 }
-.fraction-fz{
+.fraction-fz {
   font-size: 19px;
 }
 .example {
@@ -175,6 +224,12 @@ hr {
   font-size: 14px;
   vertical-align: middle;
   margin: 10px 0;
+}
+.example .example-question {
+  padding: 2px 7px;
+  color: #33a06f;
+  border-radius: 4px;
+  background-color: #f1f1f1;
 }
 .example::before {
   content: "";
